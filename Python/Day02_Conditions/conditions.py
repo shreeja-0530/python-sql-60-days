24fd73cd4ffd04f64836e9695f19904f047e70bb
Shreeja <shreeja0530@gmail.com>
2025-12-26 12:55:13 +0530

    Day 2 - Practiced if-else conditions

--- Python/Day02_Conditions/conditions.py
+++ Python/Day02_Conditions/conditions.py
@@ -1,16 +1,16 @@
-# num=int(input("Enter a number: "))
-# if num%2==0:
-#     print("Even")
-# else:
-#     print("Odd")
+num=int(input("Enter a number: "))
+if num%2==0:
+    print("Even")
+else:
+    print("Odd")
 
-# n=int(input("Enter a number: "))
-# if n<0:
-#     print("Negative")
-# elif n==0:
-#     print("Zero")
-# else:
-#     print("Positive")
+n=int(input("Enter a number: "))
+if n<0:
+    print("Negative")
+elif n==0:
+    print("Zero")
+else:
+    print("Positive")
 
 a=int(input("Enter first number: "))
 b=int(input("Enter second number: "))
